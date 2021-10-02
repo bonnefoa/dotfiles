d770a74b25472c23ac7c7e00fea4a4d319cef8fb
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2021-10-02 07:11:34 +0200

    Update nvim + ripgreprc + sway conf

--- dot_zsh_conf/env.zsh
+++ dot_zsh_conf/env.zsh
@@ -2,9 +2,10 @@ export EDITOR=vim
 
 export LESS=" -S -R"
 export GOPATH="$HOME/git-repos/golang/"
-export RIPGREP_CONFIG_PATH="$HOME/git-repos/.file/ripgreprc"
+export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/ripgreprc"
 export PATH="$HOME/git-repos/.file/bin/:$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
 
+export XDG_DATA_HOME="$HOME/.local/share"
 export PYTHONDONTWRITEBYTECODE="1"
 export LANG="en_US.utf8"
 export LC_ALL="en_US.UTF-8"
