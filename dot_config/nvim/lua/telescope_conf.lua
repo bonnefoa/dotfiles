5fb6c92e3167782375deae5eb51655079f00da1e
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2021-11-08 17:54:55 +0100

    Use fzf with telescope

--- dot_config/nvim/lua/telescope_conf.lua
+++ dot_config/nvim/lua/telescope_conf.lua
@@ -1,6 +1,14 @@
 local actions = require("telescope.actions")
 
 require("telescope").setup({
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
 	defaults = {
 		mappings = {
 			i = {
