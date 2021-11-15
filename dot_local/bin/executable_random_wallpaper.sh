a13f7e606be49afb99dc7cf1b0129862372460dc
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2021-11-15 15:57:52 +0100

    dev null stdout for random wallpaper

--- dot_local/bin/executable_random_wallpaper.sh
+++ dot_local/bin/executable_random_wallpaper.sh
@@ -4,4 +4,4 @@ set -eu
 WALLPAPER_DIR="$HOME/wallpaper/"
 
 wallpaper=$(find $WALLPAPER_DIR -maxdepth 1 -type f | shuf -n1 | sed 's/(/\\\(/g' | sed 's/)/\\\)/g' | sed "s/'/\\\'/g")
-/usr/bin/swaymsg output '*' bg "$wallpaper" fit #000000
+/usr/bin/swaymsg output '*' bg "$wallpaper" fit #000000 > /dev/null
