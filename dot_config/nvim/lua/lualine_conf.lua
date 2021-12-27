c982924dad97418c71876758cfdff0479aa299b8
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2021-12-27 12:25:58 +0100

    Update lualine

--- dot_config/nvim/lua/lualine_conf.lua
+++ dot_config/nvim/lua/lualine_conf.lua
@@ -15,7 +15,7 @@ require("lualine").setup({
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_lsp" } } },
+		lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
 		lualine_c = { { "filename", path = 1 }, "require'lsp-status'.status()" },
 
 		lualine_x = { "encoding", "fileformat", "filetype" },
