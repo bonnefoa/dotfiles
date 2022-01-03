66b60690546cfd1b7809d0c860a77c8155a27304
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2022-01-03 10:36:13 +0100

    Refactor mapping

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,11 +1,24 @@
-vim.api.nvim_set_keymap("", "<C-h>", "<C-w>h", { noremap = true })
-vim.api.nvim_set_keymap("", "<C-j>", "<C-w>j", { noremap = true })
-vim.api.nvim_set_keymap("", "<C-k>", "<C-w>k", { noremap = true })
-vim.api.nvim_set_keymap("", "<C-l>", "<C-w>l", { noremap = true })
-vim.api.nvim_set_keymap("", "<leader>/", ":nohlsearch<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>R",
-	[[<cmd>lua require('plenary.reload').reload_module('init', true)<CR>]],
-	{ noremap = true, silent = true }
-)
+local default_opts = {noremap = true, silent = false}
+
+local function set_keymap(keybind, cmd, mode, opts)
+    mode = mode or "n"
+    opts = opts or default_opts
+    vim.api.nvim_set_keymap(mode, keybind, cmd, opts)
+end
+
+local function set_lua_cmd_keymap(keybind, cmd, mode, opts)
+    set_keymap(keybind, ("<cmd>lua %s<CR>"):format(cmd), mode, opts)
+end
+
+set_lua_cmd_keymap("<leader>ff", "require('telescope.builtin').find_files()")
+set_lua_cmd_keymap("<leader>fg", "require('telescope.builtin').live_grep()")
+set_lua_cmd_keymap("<leader>fG", "Grep_from_search()")
+set_lua_cmd_keymap("<leader>ft", "require('telescope.builtin').tags()")
+set_lua_cmd_keymap("<leader>fb", "require('telescope.builtin').buffers()")
+set_lua_cmd_keymap("<leader>fB", "require('telescope.builtin').oldfiles()")
+
+set_keymap("<C-h>", "<C-w>h", "")
+set_keymap("<C-j>", "<C-w>j", "")
+set_keymap("<C-k>", "<C-w>k", "")
+set_keymap("<C-l>", "<C-w>l", "")
+set_keymap("<leader>/", ":nohlsearch<CR>", "")
--- dot_config/nvim/lua/telescope_conf.lua
+++ dot_config/nvim/lua/telescope_conf.lua
@@ -11,6 +11,9 @@ require("telescope").setup({
 	},
 	defaults = {
 		mappings = {
+			n = {
+				["q"] = actions.close,
+            },
 			i = {
 				["<C-u>"] = false,
 				["<C-d>"] = false,
@@ -26,15 +29,5 @@ function Grep_from_search()
 	local sanitized = q:gsub("\\<(.*)\\>", "%1")
 	require("telescope.builtin").grep_string({ search = sanitized, word_match = "-w" })
 end
-
---Add leader shortcuts
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<leader>sf", [[<cmd>lua require('telescope.builtin').find_files()<CR>]], opts)
-vim.api.nvim_set_keymap("n", "<leader>sg", [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], opts)
-vim.api.nvim_set_keymap("n", "<leader>sG", [[<cmd>lua Grep_from_search()<CR>]], opts)
-vim.api.nvim_set_keymap("n", "<leader>st", [[<cmd>lua require('telescope.builtin').tags()<CR>]], opts)
-vim.api.nvim_set_keymap("n", "<leader>sb", [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
-vim.api.nvim_set_keymap("n", "<leader>sB", [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], opts)
--- vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
 
 require("telescope").load_extension("fzf")
