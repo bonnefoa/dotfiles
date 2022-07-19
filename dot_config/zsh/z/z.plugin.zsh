93889d2b978e58f705d7e6a00bd5ebd57cb8009a
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2022-07-19 06:20:32 +0200

    Add zoxide z completion

---
+++ dot_config/zsh/z/z.plugin.zsh
@@ -0,0 +1,5 @@
+# Keep this file to make antigen happy
+# This will add this directory to the fpath array
+#
+# The completed command must be in a file with the "_${command}" name
+# and define a "_${command}" function
