370d57d18164e638bf928a4731a2fb44a6ac8a84
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2022-09-06 07:07:28 +0200

    Fix theme

--- dot_config/zsh/theme/theme.zsh
+++ dot_config/zsh/theme/theme.zsh
@@ -2,9 +2,8 @@ function prompt_char {
     if [ $UID -eq 0 ]; then echo "#"; else echo $; fi
 }
 
-#PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n@)%m %{$fg_bold[blue]%}%(!.%1~.%~) $(git_prompt_info)%_$(prompt_char)%{$reset_color%} '
+PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n@)%m %{$fg_bold[blue]%}%(!.%1~.%~) $(git_prompt_info)%_$(prompt_char)%{$reset_color%} '
 
-PROMPT='%{$fg_bold[blue]%}%(!.%1~.%~) %{$fg_bold[green]%}$(git_prompt_info)%{$fg_bold[blue]%}%_$(prompt_char)%{$reset_color%} '
 RPROMPT='$(date +"%H:%M:%S")'
 
 KUBE_PS1_SYMBOL_ENABLE=false
