fac108d7d2bab3671b115dd9a3e402768660ef5b
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2022-11-01 08:05:11 +0100

    Fix theme if kube_ps1 is not loaded

--- dot_config/zsh/theme/theme.zsh
+++ dot_config/zsh/theme/theme.zsh
@@ -7,7 +7,7 @@ PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n@)%m %{$fg_bold[blue]%}%(!.%1
 RPROMPT='$(date +"%H:%M:%S")'
 
 KUBE_PS1_SYMBOL_ENABLE=false
-RPROMPT="\$(kube_ps1) $RPROMPT"
+whence kube_ps1 > /dev/null && RPROMPT="\$(kube_ps1) $RPROMPT"
 
 ZSH_THEME_GIT_PROMPT_PREFIX="("
 ZSH_THEME_GIT_PROMPT_SUFFIX=") "
