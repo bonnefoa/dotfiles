92acd7e1e14b3d39dd46dc93d81cc44b80805767
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-05-07 08:46:23 +0200

    Add spell

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -16,6 +16,8 @@ vim.o.updatetime = 250
 -- Always draw sign column
 vim.wo.signcolumn = "no"
 
+vim.o.spell=true
+
 -- Don't redraw during macro apply
 vim.o.lazyredraw = true
 --Match longest first in autocompletion
