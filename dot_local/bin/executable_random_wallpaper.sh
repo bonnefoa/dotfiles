ffcbf5eb6fd4c499e6a6eab08b8ec6417d3e5dff
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2024-06-16 09:05:39 +0200

    Use swww for wallpaper

--- dot_local/bin/executable_random_wallpaper.sh
+++ dot_local/bin/executable_random_wallpaper.sh
@@ -3,5 +3,22 @@ set -eu
 
 WALLPAPER_DIR="$HOME/wallpaper/"
 
-wallpaper=$(find $WALLPAPER_DIR -maxdepth 1 -type f | shuf -n1 | sed 's/(/\\\(/g' | sed 's/)/\\\)/g' | sed "s/'/\\\'/g")
-/usr/bin/swaymsg output '*' bg "$wallpaper" fit #000000 > /dev/null
+export SWWW_TRANSITION_FPS=60
+export SWWW_TRANSITION=fade
+export SWWW_TRANSITION_DURATION=2
+export SWWW_TRANSITION_STEP=220
+
+# This controls (in seconds) when to switch to the next image
+INTERVAL=120
+
+outputs=$(swww query | awk '{print $1}' | sed 's/://')
+# wallpaper=$(find $WALLPAPER_DIR -maxdepth 1 -type f | shuf -n1 | sed 's/(/\\\(/g' | sed 's/)/\\\)/g' | sed "s/'/\\\'/g")
+
+while true; do
+	find "$WALLPAPER_DIR" -maxdepth 1 -type f \
+        | shuf \
+		| while read -r img; do
+			swww img --resize fit "$img"
+			sleep $INTERVAL
+		done
+done
