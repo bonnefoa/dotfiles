21f84cf0aca93ad3bb78256d8cac72b85427b08e
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2025-03-30 08:28:20 +0200

    Don't ignore file with picker

--- dot_config/nvim/lua/telescope_conf.lua
+++ dot_config/nvim/lua/telescope_conf.lua
@@ -11,7 +11,7 @@ require("telescope").setup({
 	},
     pickers = {
         find_files = {
-            find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+            find_command = { "fd", "--type", "f", "-H", "--strip-cwd-prefix" }
         },
     },
 	defaults = {
