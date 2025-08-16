167aa27d25bf429622df29066ec069706885ff89
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2025-08-16 10:27:42 +0200

    Add interval argument for random wallpaper

--- dot_local/bin/executable_random_wallpaper.sh
+++ dot_local/bin/executable_random_wallpaper.sh
@@ -9,16 +9,16 @@ export SWWW_TRANSITION_DURATION=2
 export SWWW_TRANSITION_STEP=220
 
 # This controls (in seconds) when to switch to the next image
-INTERVAL=300
+INTERVAL=${1:-300}
 
 outputs=$(swww query | awk '{print $1}' | sed 's/://')
 # wallpaper=$(find $WALLPAPER_DIR -maxdepth 1 -type f | shuf -n1 | sed 's/(/\\\(/g' | sed 's/)/\\\)/g' | sed "s/'/\\\'/g")
 
 while true; do
-	find "$WALLPAPER_DIR" -maxdepth 1 -type f \
-        | shuf \
-		| while read -r img; do
-			swww img --resize fit "$img"
-			sleep $INTERVAL
-		done
+  find "$WALLPAPER_DIR" -maxdepth 1 -type f |
+    shuf |
+    while read -r img; do
+      swww img -t none --resize fit "$img"
+      sleep $INTERVAL
+    done
 done
