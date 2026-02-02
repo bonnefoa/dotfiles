32fcc96c76f0aef8987b5fc4f7d9f55e6440e41c
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2026-02-02 06:32:29 +0100

    Configure trouble cascade

--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -8,25 +8,23 @@ return {
         lsp = {
           win = { position = "right" },
         },
-        mydiags = {
-          mode = "diagnostics",
-          filter = {
-            any = {
-              buf = 0,
-              {
-                severity = vim.diagnostic.severity.ERROR,
-                function(item)
-                  return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
-                end,
-              }
-            }
-          }
-        }
+        cascade = {
+          mode = "diagnostics", -- inherit from diagnostics mode
+          filter = function(items)
+            local severity = vim.diagnostic.severity.HINT
+            for _, item in ipairs(items) do
+              severity = math.min(severity, item.severity)
+            end
+            return vim.tbl_filter(function(item)
+              return item.severity == severity
+            end, items)
+          end,
+        },
       },
     },
     keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle win.size=0.40<cr>", desc = "Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0 win.size=0.40<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>xx", "<cmd>Trouble cascade toggle win.size=0.40<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble cascade toggle filter.buf=0 win.size=0.40<cr>", desc = "Buffer Diagnostics (Trouble)" },
       { "<leader>cs", "<cmd>Trouble symbols toggle focus=false win.size=0.40<cr>", desc = "Symbols (Trouble)" },
       { "<leader>cS", "<cmd>Trouble lsp toggle win.size=0.40<cr>", desc = "LSP references/definitions/... (Trouble)" },
       { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
