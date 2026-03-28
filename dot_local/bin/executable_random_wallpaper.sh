750b0bc68b0d65c2e6fdafd748d4f75aa95efdec
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2026-03-28 08:53:57 +0100

    Rename swww to awww

--- dot_local/bin/executable_random_wallpaper.sh
+++ dot_local/bin/executable_random_wallpaper.sh
@@ -3,22 +3,22 @@ set -eu
 
 WALLPAPER_DIR="$HOME/wallpaper/"
 
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION=fade
-export SWWW_TRANSITION_DURATION=2
-export SWWW_TRANSITION_STEP=220
+export AWWW_TRANSITION_FPS=60
+export AWWW_TRANSITION=fade
+export AWWW_TRANSITION_DURATION=2
+export AWWW_TRANSITION_STEP=220
 
 # This controls (in seconds) when to switch to the next image
 INTERVAL=${1:-60}
 
-outputs=$(swww query | awk '{print $1}' | sed 's/://')
+outputs=$(awww query | awk '{print $1}' | sed 's/://')
 # wallpaper=$(find $WALLPAPER_DIR -maxdepth 1 -type f | shuf -n1 | sed 's/(/\\\(/g' | sed 's/)/\\\)/g' | sed "s/'/\\\'/g")
 
 while true; do
   find "$WALLPAPER_DIR" -maxdepth 1 -type f |
     shuf |
     while read -r img; do
-      swww img -t none --resize fit "$img"
+      awww img -t none --resize fit "$img"
       sleep $INTERVAL
     done
 done
